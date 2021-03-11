b50d8f687e1c6a69ad2b6e97594426787a716084
MykhailoK007 <kotmykhailo2000@gmail.com>
2021-03-11 23:18:38 +0200

    created register page

--- src/router/UnauthenticatedRouter.tsx
+++ src/router/UnauthenticatedRouter.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Login } from '../pages/Auth/Login';
+import { Register } from '../pages/Auth/Register';
 import { NotFound } from '../pages/NotFound';
-import { Login } from '../pages/Login';
-import { SignInRoute } from './routes';
+import { SignInRoute, SignUpRoute } from './routes';
 
 export const UnauthenticatedRouter = () => (
   <Switch>
     <Route path={SignInRoute}>
       <Login />
     </Route>
-    <Route path='*'>
+    <Route path={SignUpRoute}>
+      <Register />
+    </Route>
+    <Route path='*' exact>
       <NotFound />
     </Route>
   </Switch>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,1 +1,2 @@
 export const SignInRoute = '/signin';
+export const SignUpRoute = '/signup';
