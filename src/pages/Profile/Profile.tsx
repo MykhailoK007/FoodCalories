1bdb8c8841263e90f7109e3792d4badcfa7d40a3
MykhailoK007 <kotmykhailo2000@gmail.com>
2021-04-15 09:01:04 +0300

    moved colors to constant and other remarks

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,19 +4,20 @@ import { ProfileForm } from './components/profileForm';
 import { ProfileInfo } from './components/profileInfo';
 import css from './Profile.module.scss';
 
+const inputsFields: InputProps[] = [
+  { name: 'userName', label: 'Username' },
+  { name: 'firstName', label: 'First Name' },
+  { name: 'lastName', label: 'Last Name' },
+  { name: 'email', label: 'Email', type: 'email' },
+  { name: 'avatar', label: 'Avatar' },
+  { name: 'telephone', label: 'Telephone', type: 'tel' }
+];
+
 export const Profile = () => {
   const [isEditModActive, setIsEditModActive] = useState<boolean>(false);
   const handleClick = useCallback(() => {
     setIsEditModActive(prev => !prev);
   }, [setIsEditModActive]);
-  const inputsFields: InputProps[] = [
-    { name: 'userName', label: 'Username' },
-    { name: 'firstName', label: 'First Name' },
-    { name: 'lastName', label: 'Last Name' },
-    { name: 'email', label: 'Email', type: 'email' },
-    { name: 'avatar', label: 'Avatar' },
-    { name: 'telephone', label: 'Telephone', type: 'tel' }
-  ];
   return (
     <div>
       <header className={css.contentHeader}>Profile</header>
